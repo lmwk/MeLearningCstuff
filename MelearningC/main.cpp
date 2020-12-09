f312581cba9ad4d80c6a8dac4157649016ccd1bd
lmwk <bladedshogun@gmail.com>
2020-12-08 23:25:10 -0600

    Added a little class thingy and a little tweaks to hopefully prevent dataloss

--- MelearningC/main.cpp
+++ MelearningC/main.cpp
@@ -5,32 +5,35 @@
 #include <chrono>
 #include <conio.h>
 
-double assingnsubjectid(char name[]);
+size_t assingnsubjectid(char name[]);
 
 using namespace std;
 using namespace this_thread;
 using namespace chrono;
 int main()
 {
-    int subjectid;
+    size_t subjectid;
     char subjectname[] = "Placeholder";
-
-
 
     cout << "Enter a subject name: ";
     cin >> subjectname;
     subjectid = assingnsubjectid(subjectname);
-    char key;
-    cout << "\nThe subject id of: " << subjectname << " is " << subjectid << "\n";
+
+    char idclass[] = "PlaceHolder";
+
+    cout << "Type the class you would want to be in (Selections are, A, B, C, D and, wow you're bad at school, F) \n";
+    cin >> idclass;
+
+    cout << "\nThe subject id of: " << subjectname << " is " << subjectid << " and the class this 'profound' individual is in is called " << idclass << "\n";
     seconds dura(5);
     sleep_for(dura);
     return 0;
 }
 
-double assingnsubjectid(char name[])
+size_t assingnsubjectid(char name[])
 {
     srand(time(0));
-    int rande = strlen(name);
+    size_t rande = strlen(name);
 
-    return rand() * (double)rande * (double)rande * rande;
+    return rand() * rande * rande * rande;
 }
